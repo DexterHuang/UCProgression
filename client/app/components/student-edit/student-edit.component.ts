2fd6643c17f388f076373eb9f234e03d1dee3516
DexterHuang <ching.huang.dexter@gmail.com>
2016-12-22 22:36:33 +0800

    inital

---
+++ client/app/components/student-edit/student-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'student-edit',
+    templateUrl: 'student-edit.component.html',
+    styleUrls: ['student-edit.component.css']
+})
+export class StudentEditComponent {
+
+}
